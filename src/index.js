1558caee1c991caad7e6db246c586232f4d8e1bf
Pete Turnbull <pete.j.turnbull@gmail.com>
2018-07-07 08:04:22 +0100

    add name to error

--- src/index.js
+++ src/index.js
@@ -5,7 +5,10 @@ module.exports = environment => async (ctx, next) => {
     ctx.status = err.output ? err.output.statusCode || 500 : 500
     ctx.body =
       environment === 'production'
-        ? { error: err.message }
-        : { error: err.message, stack: err.stack }
+        ? { error: err.name ? `${err.name}: ${err.message}` : err.message }
+        : {
+            error: err.name ? `${err.name}: ${err.message}` : err.message,
+            stack: err.stack,
+          }
   }
 }
